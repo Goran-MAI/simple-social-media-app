3fa36339a8bef389bba319cb58ba61f332e654cf
Goran Janosevic <goran-janosevic@hotmail.com>
2025-11-11 03:55:57 +0100

    minor fixes for PR-workflow

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,10 +1,16 @@
 # backend/init_db.py
+import os
 from sqlmodel import SQLModel, create_engine
 from backend.models.user import User
 from backend.models.post import Post
 
 # PostgreSQL connection URL
-DATABASE_URL = "postgresql+psycopg2://ssma_admin:ssma_2025@localhost/ssma_db"
+# DATABASE_URL = "postgresql+psycopg2://ssma_admin:ssma_2025@localhost/ssma_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://ssma_admin:ssma_2025@localhost:5432/ssma_db"
+)
+
 
 # Create database engine
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True zeigt SQL-Statements
