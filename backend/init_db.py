5f81aaf7e83127af1a9922e0146c1766d2fb1f3d
Goran Janosevic <goran-janosevic@hotmail.com>
2025-11-12 18:52:58 +0100

    changes on yaml file

--- backend/init_db.py
+++ backend/init_db.py
@@ -10,17 +10,7 @@ from backend.models.post import Post
 default_url = "postgresql+psycopg2://ssma_admin:ssma_2025@localhost/ssma_db"
 
 DATABASE_URL = os.getenv("DATABASE_URL", default_url)
-print(DATABASE_URL)
-
-# different URLS for local and GitHub (CI)
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL is None:
-    # local
-    DATABASE_URL = "postgresql+psycopg2://ssma_admin:ssma_pass@localhost:5432/ssma_test"
-else:
-    # CI-Environment (GitHub Actions)
-    #DATABASE_URL = DATABASE_URL
-    DATABASE_URL = "postgresql+psycopg2://ssma_admin:ssma_pass@postgres:5432/ssma_test"
+print(f"############################ {DATABASE_URL} #############################")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
